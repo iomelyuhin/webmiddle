8f3e9f7f0ffdd8ae7726d7dc950cc0a7e4365d65
iomelyuhin <i.omelyuhin@gmail.com>
2019-06-01 17:34:46 +0300

    webmiddle v.7.2 feeds

--- js/main.js
+++ js/main.js
@@ -107,6 +107,16 @@ for (let i = 0; i < menuItemArr.length; i++) {
 
 var feedItems = document.querySelectorAll('.feed__item');
 var feedAvatars = document.querySelectorAll('.feed__ava-item');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  feedItems[currentSlide].classList.remove('active');
+  feedAvatars[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % feedItems.length;
+  feedItems[currentSlide].classList.add('active');
+  feedAvatars[currentSlide].classList.add('active');
+}
 
 for (let i = 0; i < feedAvatars.length; i++) {
   const feedAvatar = feedAvatars[i];
@@ -132,7 +142,10 @@ for (let i = 0; i < feedAvatars.length; i++) {
       target.classList.contains('feed__ava-img')) {
       feedAvatar.classList.add('active');
       feedItem.classList.add('active');
+      currentSlide = i;
     }
 
   });
 }
+
+
